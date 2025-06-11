74b09c3e8edb95817263ff2f126071dbbf24d222
Shashank Shekhar <shashankshekhar8534@gmail.com>
2025-06-12 01:43:12 +0530

    project

---
+++ usermanagement/users/serializers.py
@@ -0,0 +1,33 @@
+# users/serializers.py
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from .models import User
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'first_name', 'last_name', 'phone', 'user_type']
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            phone=validated_data['phone'],
+            user_type=validated_data['user_type'],
+        )
+        user.set_password(validated_data['password'])  # use a real password field
+        user.save()
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(email=data['email'], password=data['password'])
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+        return user
