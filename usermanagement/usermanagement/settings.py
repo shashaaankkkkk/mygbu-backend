539996ab7fa97fbd495753362a7caa0cf01bc2f1
Shashank Shekhar <shashankshekhar8534@gmail.com>
2025-07-03 13:03:23 +0530

    cors

--- usermanagement/usermanagement/settings.py
+++ usermanagement/usermanagement/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+CORS_ALLOW_CREDENTIALS = True
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
     'users',
    'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
+    'django.middleware.common.CommonMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
